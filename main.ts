9c152c42dd5456d723a041fa0141351affab4616
Aorkado <144384724+Aorkado@users.noreply.github.com>
2023-11-06 21:13:47 +0100

    Update main.ts to v.6.11.2023

--- main.ts
+++ main.ts
@@ -1,56 +1,15 @@
-function correctPath (FLL2: number) {
-    if (FLL2 == 1) {
-        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 40)
-        DFRobotMaqueenPlus.mototStop(Motors.M1)
-    } else {
-        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 40)
-        DFRobotMaqueenPlus.mototStop(Motors.M2)
-    }
-}
-function turningRight () {
-    basic.showNumber(3)
-    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 40)
-    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, 40)
-    while (FLR2 && FLR1) {
-        Sensors()
-    }
-    while (!(FLR2 && FLR1)) {
-        Sensors()
-    }
-    DFRobotMaqueenPlus.mototStop(Motors.ALL)
-    basic.showNumber(0)
-}
-function Sensors () {
-    FLL1 = DFRobotMaqueenPlus.readPatrol(Patrol.L1)
-    FLL2 = DFRobotMaqueenPlus.readPatrol(Patrol.L2)
-    FLL3 = DFRobotMaqueenPlus.readPatrol(Patrol.L3)
-    FLR1 = DFRobotMaqueenPlus.readPatrol(Patrol.R1)
-    FLR2 = DFRobotMaqueenPlus.readPatrol(Patrol.R2)
-    FLR3 = DFRobotMaqueenPlus.readPatrol(Patrol.R3)
-    ultrasound = DFRobotMaqueenPlus.ultraSonic(PIN.P2, PIN.P1)
-}
-function turningLeft () {
-    basic.showNumber(4)
-    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 40)
-    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CCW, 40)
-    while (FLL1 && FLR1) {
-        Sensors()
-    }
-    while (!(FLL1 && FLR1)) {
-        Sensors()
-    }
-    DFRobotMaqueenPlus.mototStop(Motors.ALL)
-    basic.showNumber(0)
-}
-function followStraightLine () {
+function followLine () {
     basic.showNumber(0)
     Sensors()
     while (stateOfMovement == 0) {
-        if (FLL2 || FLR2) {
+        if (FLR2 || FLL2) {
             correctPath(FLL2)
         }
+        if (ultrasound < 5) {
+            turn(FLR2)
+        }
         while (FLL1 && FLR1) {
-            if (ultrasound < 7) {
+            if (ultrasound < 5) {
                 turn(FLR2)
             }
             if (FLR2) {
@@ -64,33 +23,72 @@ function followStraightLine () {
     }
     DFRobotMaqueenPlus.mototStop(Motors.ALL)
 }
+function correctPath (Left: number) {
+    if (Left == 1) {
+        DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 40)
+        DFRobotMaqueenPlus.mototStop(Motors.M1)
+    } else {
+        DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 40)
+        DFRobotMaqueenPlus.mototStop(Motors.M2)
+    }
+}
+function turningRight () {
+    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 40)
+    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CCW, 40)
+    while (FLL1 && FLR1) {
+        Sensors()
+    }
+    while (!(FLL1 && FLR1)) {
+        Sensors()
+    }
+}
+function Sensors () {
+    FLL1 = DFRobotMaqueenPlus.readPatrol(Patrol.L1)
+    FLL2 = DFRobotMaqueenPlus.readPatrol(Patrol.L2)
+    FLL3 = DFRobotMaqueenPlus.readPatrol(Patrol.L3)
+    FLR1 = DFRobotMaqueenPlus.readPatrol(Patrol.R1)
+    FLR2 = DFRobotMaqueenPlus.readPatrol(Patrol.R2)
+    FLR3 = DFRobotMaqueenPlus.readPatrol(Patrol.R3)
+    ultrasound = DFRobotMaqueenPlus.ultraSonic(PIN.P2, PIN.P1)
+}
+function turningLeft () {
+    DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 40)
+    DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CCW, 40)
+    while (FLL1 && FLR1) {
+        Sensors()
+    }
+    while (!(FLL1 && FLR1)) {
+        Sensors()
+    }
+}
 function turn (turnRight: number) {
     if (turnRight == 1) {
+        DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 30)
         while (!(FLR3)) {
-            DFRobotMaqueenPlus.mototRun(Motors.ALL, Dir.CW, 30)
+            if (!(FLL1 && FLR1)) {
+                break;
+            }
             Sensors()
         }
         basic.pause(800)
         DFRobotMaqueenPlus.mototStop(Motors.ALL)
-        basic.showNumber(1)
         turningRight()
     } else {
         DFRobotMaqueenPlus.mototStop(Motors.ALL)
-        basic.showNumber(2)
         turningLeft()
     }
 }
-let ultrasound = 0
 let FLR3 = 0
 let FLL3 = 0
-let FLL2 = 0
-let FLL1 = 0
 let FLR1 = 0
+let FLL1 = 0
+let ultrasound = 0
+let FLL2 = 0
 let FLR2 = 0
 let stateOfMovement = 0
 stateOfMovement = 0
 DFRobotMaqueenPlus.PID(PID.OFF)
 basic.forever(function () {
     basic.showNumber(0)
-    followStraightLine()
+    followLine()
 })
